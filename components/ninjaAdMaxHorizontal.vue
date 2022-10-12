6075e54fba84855bcd9d6adcc9bcdf7e2ddb54d2
naoto <naotomain@gmail.com>
2022-10-13 00:30:22 +0900

    SP向け広告をちょっとマシにした

--- components/ninjaAdMaxHorizontal.vue
+++ components/ninjaAdMaxHorizontal.vue
@@ -5,18 +5,28 @@
 <script>
 export default {
   async mounted() {
-    const iframe = document.createElement('iframe');
-    iframe.setAttribute("width",800);
-    iframe.setAttribute("height",100);
-    iframe.setAttribute("frameborder","no");
-    iframe.setAttribute("scrolling","no");
-    const head = document.getElementsByTagName('head')[0];
-    this.$refs.ads.appendChild(iframe);
-    const html = '<script src="https://adm.shinobi.jp/s/8e3a80fb201e787e93fd822010d54864"><\/script>';
-    const iframeDocument = iframe.contentWindow.document;
-    iframeDocument.open();
-    iframeDocument.write(html);
-    iframeDocument.close();
+    const iframePc = document.createElement('iframe');
+    iframePc.setAttribute("width",800);
+    iframePc.setAttribute("height",100);
+    iframePc.setAttribute("class","d-none d-md-inline");
+    iframePc.setAttribute("frameborder","no");
+    iframePc.setAttribute("scrolling","no");
+    this.$refs.ads.appendChild(iframePc);
+    const iframePcDocument = iframePc.contentWindow.document;
+    iframePcDocument.open();
+    iframePcDocument.write('<script src="https://adm.shinobi.jp/s/8e3a80fb201e787e93fd822010d54864"><\/script>');
+    iframePcDocument.close();
+    const iframeSp = document.createElement('iframe');
+    iframeSp.setAttribute("width",400);
+    iframeSp.setAttribute("height",75);
+    iframeSp.setAttribute("class","d-inline d-sm-none");
+    iframeSp.setAttribute("frameborder","no");
+    iframeSp.setAttribute("scrolling","no");
+    this.$refs.ads.appendChild(iframeSp);
+    const iframeSpDocument = iframeSp.contentWindow.document;
+    iframeSpDocument.open();
+    iframeSpDocument.write('<script src="https://adm.shinobi.jp/s/46f1626a9e53827a5bfec9f6b40b5337"><\/script>');
+    iframeSpDocument.close();
   }
 }
 </script>
